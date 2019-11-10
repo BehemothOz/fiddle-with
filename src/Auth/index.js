eed4ae825a5a502342d977db81450f4f5a9d192e
behemothoz <behemothoz@yandex.ru>
2019-11-10 17:33:43 +0300

    tesing connect component

---
+++ src/Auth/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { FullHeight, Paper, Button } from '../shared';
+
+const Auth = () => {
+    return (
+        <FullHeight>
+            <Paper>
+                <Button>Some button</Button>
+            </Paper>
+        </FullHeight>
+    )
+}
+
+export default Auth;
