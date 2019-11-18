bb683fce795dd35405e53998625e1272ac04114e
behemothoz <behemothoz@yandex.ru>
2019-11-18 21:59:12 +0300

    temp remove schema

--- api/routes/registration.js
+++ api/routes/registration.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/user');
+// const User = require('../models/user');
 
 router.post('/', async (req, res) => {
-    const { name, email, password } = req.body;
-    console.log(req.body)
+    // const { name, email, password } = req.body;
     // const user = new User({
     //     name: 'Ivan',
     //     email: 'ivan@main.com',
@@ -13,7 +12,8 @@ router.post('/', async (req, res) => {
 
     try {
         // const newUser = await user.save();
-        res.status(200).json(newUser);
+        // res.status(200).json(newUser);
+        res.status(200).json({});
     } catch (error) {
         res.status(400).json({ message: error.message });
     }
