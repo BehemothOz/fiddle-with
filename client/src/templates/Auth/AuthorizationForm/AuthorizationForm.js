d816b7cd20cee3a3a1dbc345c481b9c53ab9290a
behemothoz <behemothoz@yandex.ru>
2019-11-20 22:33:39 +0300

    connect formik to AuthorizationForm

--- client/src/templates/Auth/AuthorizationForm/AuthorizationForm.js
+++ client/src/templates/Auth/AuthorizationForm/AuthorizationForm.js
@@ -1,49 +1,22 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { Form, Icon, Input } from 'antd';
-import { Button } from '../../../shared';
-import styles from './styles.module.scss';
+import { withFormik } from 'formik';
+import View from './View';
 
-const AuthorizationForm = () => {
-    const handleSubmit = () => {};
+const mapPropsToValues = () => ({
+    email: '',
+    password: ''
+});
 
-    const post = () => {
-        fetch('http://localhost:4444/registration', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json;charset=utf-8'
-            },
-            body: JSON.stringify({ name: 'Name', email: 'email@mail.com', password: '1234' })
-        }).then(r => console.log(r))
-    }
+const handleSubmit = (values, bag) => {
+    setTimeout(() => {
+        console.info(JSON.stringify(values, null, 2));
+        bag.setSubmitting(false);
+    }, 1000);
+};
 
-    return (
-        <Form onSubmit={handleSubmit}>
-            <Form.Item>
-                <Input
-                    prefix={<Icon type="user" className={styles.iconInput} />}
-                    placeholder="Username"
-                />
-            </Form.Item>
-            <Form.Item>
-                <Input.Password
-                    prefix={<Icon type="lock" className={styles.iconInput} />}
-                    type="password"
-                    placeholder="Password"
-                />
-            </Form.Item>
-            <div className={styles.buttons}>
-                <Button type="primary" htmlType="submit" className={styles.loginButton} block>
-                    Log in
-                </Button>
-                <Button type="primary" onClick={post} className={styles.loginButton} block>
-                    Log in
-                </Button>
-                <Link to="/registration" className={styles.link}>Registration</Link>
-                <Link to="/home" className={styles.link}>temp home</Link>
-            </div>
-        </Form>
-    )
-}
+const displayName = 'AuthorizationForm';
 
-export default AuthorizationForm;
+export default withFormik({
+    mapPropsToValues,
+    handleSubmit,
+    displayName
+})(View);
---
+++ client/src/templates/Auth/AuthorizationForm/View.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Form, Icon } from 'antd';
+import { Button, TextField } from '../../../shared';
+import styles from './styles.module.scss';
+
+const AuthorizationForm = props => {
+    const { handleSubmit } = props;
+
+    const post = () => {
+        fetch('http://localhost:4444/registration', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8'
+            },
+            body: JSON.stringify({ name: 'Name', email: 'email@mail.com', password: '1234' })
+        }).then(r => console.log(r))
+    }
+
+    return (
+        <Form onSubmit={handleSubmit}>
+            <Form.Item>
+                <TextField
+                    name="email"
+                    prefix={<Icon type="user" className={styles.iconInput} />}
+                    placeholder="Username"
+                />
+            </Form.Item>
+            <Form.Item>
+                <TextField
+                    name="password"
+                    type="password"
+                    prefix={<Icon type="lock" className={styles.iconInput} />}
+                    placeholder="Password"
+                />
+            </Form.Item>
+            <div className={styles.buttons}>
+                <Button type="primary" htmlType="submit" className={styles.loginButton} block>
+                    Log in
+                </Button>
+                <Button type="primary" onClick={post} className={styles.loginButton} block>
+                    Log in
+                </Button>
+                <Link to="/registration" className={styles.link}>Registration</Link>
+                <Link to="/home" className={styles.link}>temp home</Link>
+            </div>
+        </Form>
+    )
+}
+
+export default AuthorizationForm;
