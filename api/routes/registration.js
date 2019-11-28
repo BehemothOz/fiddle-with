b6f4380b0eff5fab90b67fc5d0fd1cfb8f05d828
behemothoz <behemothoz@yandex.ru>
2019-11-28 23:28:30 +0300

    connect to db reqistration route

--- api/routes/registration.js
+++ api/routes/registration.js
@@ -1,21 +1,17 @@
 const express = require('express');
 const router = express.Router();
-// const User = require('../models/user');
+const User = require('../models/user');
 
 router.post('/', async (req, res) => {
-    // const { name, email, password } = req.body;
-    // const user = new User({
-    //     name: 'Ivan',
-    //     email: 'ivan@main.com',
-    //     password: '12345qwer'
-    // });
-
+    const { name, email, password } = req.body;
+    const user = new User({
+        name,
+        email,
+        password
+    });
     try {
-        // const newUser = await user.save();
-        // res.status(200).json(newUser);
-        console.log(req.body)
-
-        res.status(200).json({});
+        const newUser = await user.save();
+        res.status(200).json(newUser);
     } catch (error) {
         res.status(400).json({ message: error.message });
     }
