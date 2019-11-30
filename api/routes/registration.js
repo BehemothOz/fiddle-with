e4b54ef1e86ebeff5b8db26c5cdeaaeabebfbb8b
behemothoz <behemothoz@yandex.ru>
2019-11-30 18:48:22 +0300

    update registration route

--- api/routes/registration.js
+++ api/routes/registration.js
@@ -1,19 +1,35 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
+const validateRegistration = require('./validation');
+const [ USERNAME, EMAIL, PASSWORD ] = require('../constants/auth');
 
-router.post('/', async (req, res) => {
+router.post('/', async (req, res, next) => {
+    // Validate the data
+    const { error } = validateRegistration(req.body);
+    if (error) return next({ status: 400, type: 'validation', body: error });
+
     const { name, email, password } = req.body;
+
+    // Checking if the user is already in the database
+    const existUser = await User.findOne({ [EMAIL]: email });
+    if (existUser) return next({ status: 400, body: 'Email already exists'});
+
+    // TODO: add password hashing option
+
+    // Create a new user
     const user = new User({
-        name,
-        email,
-        password
+        [USERNAME]: name,
+        [EMAIL]: email,
+        [PASSWORD]: password
     });
+
     try {
         const newUser = await user.save();
         res.status(200).json(newUser);
     } catch (error) {
-        res.status(400).json({ message: error.message });
+        // res.status(400).json({ message: error.message });
+        next({ status: 400, body: error.message })
     }
 })
 
