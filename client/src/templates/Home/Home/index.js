5bb49de1a6937b6523ceda891987ce3e76f42855
behemothoz <behemothoz@yandex.ru>
2019-12-18 23:23:16 +0300

    testing hook for fetch

--- client/src/templates/Home/Home/index.js
+++ client/src/templates/Home/Home/index.js
@@ -1,5 +1,62 @@
 import React from 'react';
+import { Spin } from 'antd';
 
-const Home = () => <h4>Hello, world!</h4>
+const initialState = {
+    data: { count: 0 },
+    fetching: false,
+    error: null
+}
+
+const useAction = action => {
+    const [state, setState] = React.useState(initialState);
+
+    const performAction = async (body = {}) => {
+        try {
+            setState({ ...state, fetching: true });
+
+            const data = await action(body);
+            setState({ ...state, data: { count: state.data.count + data }, fetching: false });
+        } catch (error) {
+            setState({ ...state, error, fetching: false });
+        } finally {
+            console.log('finally')
+        }
+    }
+
+    return [state, performAction];
+}
+
+
+const callPromise = data => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log('time end!')
+            resolve(data);
+        }, 2000)
+    })
+}
+
+const Home = () => {
+    const [{ data, fetching }, perform] = useAction(callPromise);
+
+    console.log({data, fetching});
+
+    const click = () => {
+        console.count('click');
+        perform(1)
+    }
+
+    const getNumber = () => {
+        return data ? data.count : 'unknown number'
+    }
+
+    return (
+        <div style={{ padding: '20px' }}>
+            <button onClick={click} style={{ marginRight: 10 }}>Set Name 'Ivan'</button>
+            {fetching && <Spin />}
+            <h4>Hello, world, {getNumber()}!</h4>
+        </div>
+    )
+}
 
 export default Home;
