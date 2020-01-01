a08ab89d6a4ca2721e871763579c529f9a5ea5ea
d.davydov <behemothoz@yandex.ru>
2020-01-01 20:45:17 +0300

    onSuccess for registration

--- client/src/templates/Auth/RegistrationPage/RegistrationPage.js
+++ client/src/templates/Auth/RegistrationPage/RegistrationPage.js
@@ -1,18 +1,28 @@
 import React, { useCallback } from 'react';
 import RegistrationForm from '../RegistrationForm';
+import RegistrationComplete from '../../../components/RegistrationComplete';
 import useSubmit from '../../../hooks/async/useSubmit';
 import api from '../../../api';
 
 const RegistrationPage = () => {
+    const [complete, setComplete] = React.useState(false);
+
     const handleSubmit = useCallback(options => {
         return api.post('/registration', options);
     });
 
     const handleSubmitSuccess = useCallback(() => {
         console.log('Success submit form!');
+        setComplete(true);
     });
 
     const [state, submit] = useSubmit(handleSubmit);
+
+    if (complete) {
+        return (
+            <RegistrationComplete />
+        )
+    }
 
     return (
         <RegistrationForm
