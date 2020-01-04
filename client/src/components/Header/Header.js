2a6048ace80b9f3180b1fbf70c3a2a181580d61a
d.davydov <behemothoz@yandex.ru>
2020-01-04 15:38:03 +0300

    add Header component

---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,18 @@
+import React, { useContext } from 'react';
+import cn from 'classnames';
+import { Switch } from '../../shared';
+import { ThemeStateContext } from '../../contexts';
+import styles from './styles.module.scss';
+
+const Header = props => {
+    console.count('<HEADER /> RENDER');
+    const theme = useContext(ThemeStateContext);
+
+    return (
+        <header className={cn(styles.root, styles[`theme-${theme}`])}>
+            {props.children}
+        </header>
+    )
+}
+
+export default Header;
