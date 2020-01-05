654c3212d07e677d7a02c400f560ed9a2dc044d6
d.davydov <behemothoz@yandex.ru>
2020-01-05 15:10:49 +0300

    clear home page

--- client/src/templates/BaseTemplate/BaseTemplate.js
+++ client/src/templates/BaseTemplate/BaseTemplate.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import Header from '../../components/Header';
+import { Switch } from '../../shared';
 import HomePage from '../../pages/HomePage';
 
 const BaseTemplate = () => {
     console.count('<BaseTemplate /> RENDER');
-    return <HomePage />
+    return (
+        <>
+            <Header><Switch /></Header>
+            <HomePage />
+        </>
+    )
 }
 
 export default BaseTemplate;
