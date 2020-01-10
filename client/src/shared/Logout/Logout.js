e5ca1dc913ccc7fd190186c946ff62bc6153b55b
behemothoz <behemothoz@yandex.ru>
2020-01-11 00:09:35 +0300

    add Logout button

---
+++ client/src/shared/Logout/Logout.js
@@ -0,0 +1,19 @@
+import React, { useContext } from 'react';
+import { Button } from 'antd';
+import { AuthActionsContext } from '../../contexts/AuthContext';
+
+const Logout = () => {
+    console.count('<Logout /> RENDER');
+    const auth = useContext(AuthActionsContext);
+
+    return (
+        <Button
+            icon="poweroff"
+            onClick={auth.logout}
+        >
+            Logout
+        </Button>
+    )
+}
+
+export default Logout;
