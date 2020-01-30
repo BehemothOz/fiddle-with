b4be521259b92c66d6bf563a32d3499587fc2e4a
behemothoz <behemothoz@yandex.ru>
2020-01-30 11:56:58 +0300

    for deploy heroku

--- client/src/api/config.js
+++ client/src/api/config.js
@@ -1,5 +1,10 @@
+const api = {
+    development: 'http://localhost:4444',
+    production: 'https://fiddle-wth-api.herokuapp.com'
+};
+
 const config = {
-    url: 'http://localhost:4444',
+    url: process.env.NODE_ENV === 'production' ? api.production : api.development,
     timeout: 30000
 }
 
