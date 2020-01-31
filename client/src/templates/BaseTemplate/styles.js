a0e25082e51682dc51d23620bf89d062b87da42f
behemothoz <behemothoz@yandex.ru>
2020-01-31 20:58:08 +0300

    padding bottom for layout

--- client/src/templates/BaseTemplate/styles.js
+++ client/src/templates/BaseTemplate/styles.js
@@ -2,7 +2,8 @@ import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles(theme => ({
     layout: {
-        paddingTop: theme.spacing(2)
+        paddingTop: theme.spacing(2),
+        paddingBottom: theme.spacing(8)
     },
     title: {
         flexGrow: 1
