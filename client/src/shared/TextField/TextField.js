c625bea7f66cf2c014985e52a48f20d9be85f107
behemothoz <behemothoz@yandex.ru>
2020-01-31 18:41:25 +0300

    autoComplete off for field form

--- client/src/shared/TextField/TextField.js
+++ client/src/shared/TextField/TextField.js
@@ -13,6 +13,7 @@ const TextField = props => {
             {...rest}
             error={error}
             helperText={helperText}
+            autoComplete='off'
             fullWidth
         />
     )
