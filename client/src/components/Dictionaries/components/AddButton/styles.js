4f174dc450f8d6a03a00e2b2bb076d1f1e4800eb
behemothoz <behemothoz@yandex.ru>
2020-01-31 18:38:06 +0300

    fix add dictionary button

--- client/src/components/Dictionaries/components/AddButton/styles.js
+++ client/src/components/Dictionaries/components/AddButton/styles.js
@@ -2,7 +2,7 @@ import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles(theme => ({
     fab: {
-        position: 'absolute',
+        position: 'fixed',
         bottom: theme.spacing(2),
         right: theme.spacing(2),
     },
