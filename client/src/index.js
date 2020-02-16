fbae3dcdc794dcefc2db22a10668b066a407fcf8
behemothoz <behemothoz@yandex.ru>
2020-02-16 11:59:24 +0300

    remove extra files

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from "react-router-dom";
-import './assets/styles/index.scss';
 
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 import ThemeProvider from './providers/ThemeProvider';
 import AuthProvider from './providers/AuthProvider';
@@ -19,8 +17,3 @@ ReactDOM.render(
     </Router>,
     document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
